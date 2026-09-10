05c021ab3be16c07852d0e01f486f6cbda4b9078
haroldoradialista-cmd <haroldoradialista@gmail.com>
2026-09-09 21:28:27 -0300

    Add files via upload

--- lib/paginas/pedidos_page.dart
+++ lib/paginas/pedidos_page.dart
@@ -265,17 +265,6 @@ class PedidoMusicaPage extends StatefulWidget {
 }
 
 class _PedidoMusicaPageState extends State<PedidoMusicaPage> {
-  @override
-  void initState() {
-    super.initState();
-    // Traz o nome e o WhatsApp do cadastro: o ouvinte não digita de novo.
-    if (CadastroService.nome.isNotEmpty) _nome.text = CadastroService.nome;
-    if (CadastroService.whatsapp.isNotEmpty) {
-      _whatsapp.text = CadastroService.whatsapp;
-    }
-    _cidade.addListener(_filtrarCidades);
-  }
-
   final _nome = TextEditingController();
   final _musica = TextEditingController();
   final _interprete = TextEditingController();
@@ -284,6 +273,16 @@ class _PedidoMusicaPageState extends State<PedidoMusicaPage> {
   List<String> _cidadesDoEstado = [];
   List<String> _sugestoes = [];
   bool _carregandoCidades = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Traz o nome e o WhatsApp do cadastro: o ouvinte não digita de novo.
+    if (CadastroService.nome.isNotEmpty) _nome.text = CadastroService.nome;
+    if (CadastroService.whatsapp.isNotEmpty) {
+      _whatsapp.text = CadastroService.whatsapp;
+    }
+  }
 
   /// Baixa as cidades do estado escolhido (lista oficial do IBGE)
   Future<void> _carregarCidadesDoEstado(String uf) async {
